c6aadd1708a6b4d216f8b1a7eeed0cba66fec4c1
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2022-08-26 20:32:35 +0100

    Add copyright and reduce continuation indent

--- test/test_config.c
+++ test/test_config.c
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2022 Marc Ransome
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
@@ -5,7 +27,7 @@
 #include "config.h"
 
 #define TEST_PROGRAM_NAME "flog"
-#define TEST_MESSAGE      "test message"
+#define TEST_MESSAGE "test message"
 
 void flog_usage() {}
 void flog_version() {}
@@ -39,9 +61,9 @@ int main(void) {
     cmocka_set_message_output(CM_OUTPUT_TAP);
 
     const struct CMUnitTest tests[] = {
-            cmocka_unit_test(test_new_config_with_zero_arg_count_calls_assert),
-            cmocka_unit_test(test_new_config_with_null_arg_values_calls_assert),
-            cmocka_unit_test(test_new_config_returns_non_null)
+        cmocka_unit_test(test_new_config_with_zero_arg_count_calls_assert),
+        cmocka_unit_test(test_new_config_with_null_arg_values_calls_assert),
+        cmocka_unit_test(test_new_config_returns_non_null)
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
