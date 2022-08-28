72fbec0544256deb594682e916d4ebdfffaaed42
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2022-08-28 22:36:32 +0100

    Minor indentation fix

--- test/test_config.c
+++ test/test_config.c
@@ -49,7 +49,7 @@ static void test_new_config_with_zero_arg_count_calls_assert(void **state) {
 }
 
 static void test_new_config_with_null_arg_values_calls_assert(void **state) {
-    int mock_argc = 1;      // should pass >0 assertion
+    int mock_argc = 1;       // should pass >0 assertion
     char **mock_argv = NULL; // should fail non-null assertion
 
     expect_assert_failure(flog_config_new(mock_argc, mock_argv));
