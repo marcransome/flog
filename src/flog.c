35b3c082200ed6571e47245c5424306a78016f92
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2024-11-30 11:56:10 +0000

    Consolidate unit test macros and external declarations

--- src/flog.c
+++ src/flog.c
@@ -29,23 +29,7 @@
 #include "config.h"
 
 #ifdef UNIT_TESTING
-extern void mock_assert(const int result, const char * const expression,
-    const char * const file, const int line);
-
-#undef assert
-#define assert(expression) \
-    mock_assert((int)(expression), #expression, __FILE__, __LINE__);
-
-extern void * _test_malloc(const size_t size, const char *file, const int line);
-extern void * _test_realloc(void *ptr, const size_t size, const char *file, const int line);
-extern void * _test_calloc(const size_t number_of_elements, const size_t size,
-                   const char *file, const int line);
-extern void _test_free(void * const ptr, const char *file, const int line);
-
-#define malloc(size) _test_malloc(size, __FILE__, __LINE__)
-#define realloc(ptr, size, file, line) _test_realloc(ptr, size, __FILE__, __LINE__)
-#define calloc(num, size) _test_calloc(num, size, __FILE__, __LINE__)
-#define free(ptr) _test_free(ptr, __FILE__, __LINE__)
+#include "testing.h"
 #endif
 
 #define OS_LOG_FORMAT_PUBLIC "%{public}s"
