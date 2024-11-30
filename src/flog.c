46ac268f2de2e6faf8c12a49f4e42faa14fc84a4
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2024-11-30 11:39:10 +0000

    Add realloc macro for leak detection

--- src/flog.c
+++ src/flog.c
@@ -43,6 +43,7 @@ extern void * _test_calloc(const size_t number_of_elements, const size_t size,
 extern void _test_free(void * const ptr, const char *file, const int line);
 
 #define malloc(size) _test_malloc(size, __FILE__, __LINE__)
+#define realloc(ptr, size, file, line) _test_realloc(ptr, size, __FILE__, __LINE__)
 #define calloc(num, size) _test_calloc(num, size, __FILE__, __LINE__)
 #define free(ptr) _test_free(ptr, __FILE__, __LINE__)
 #endif
